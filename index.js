7610478078b99230bb1a6bcab679a3168b618398
Arthur Hermann <arthurhermannc@gmail.com>
2021-10-18 10:09:18 -0300

    add: ambient prepare

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+require('dotenv').config();
+
+const app = express();
+app.use(bodyParser.json());
+
+const PORT = process.env.PORT || 3000;
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
+});
+
+app.use('/products', require('./router/productsRouter'));
+
+app.listen(PORT, () => {
+  console.log('App listening on port 3000!');
 });
